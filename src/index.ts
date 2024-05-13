9f55ea71133c8afb95b0b1ba5080e7bdcf0f4c36
Gerard Villalobos <gvillalobos@gomultitaskr.com>
2024-05-12 18:52:16 -0700

    Added Dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from "express";
 import router from "./routes/api";
 import cors from "cors";
 import { ErrorHandler } from "./middlewares/ErrorHandler";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 app.use(ErrorHandler);
-const PORT = 3000;
+
+const PORT = process.env.APP_PORT;
+
 app.listen(PORT, async () => {
   console.log("Server Running");
 });
